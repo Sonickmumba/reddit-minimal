3102068ec3719982382f22b7cd2f7933ce50ee37
Sonick Mumba <kingellie.mumba@gmail.com>
2024-05-15 19:11:05 +0200

    Add css to the logo of the header

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -2,8 +2,17 @@ import React from 'react';
 import styles from './Header.module.css';
 
 const Header = () => (
-  <div className={styles.headers}>
-    <p>Mumba</p>
+  <div className={styles.header}>
+    <div className={styles.logo}>
+      <div>
+        <span className={styles.reddit}>Reddit</span>
+        Minimal
+      </div>
+    </div>
+    <div className={styles.search}>
+      <input type="text" placeholder="Search..." />
+      <button type="button">Search</button>
+    </div>
   </div>
 );
 
