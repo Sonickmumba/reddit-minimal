9699789989e3115ad017b404dad6d96bb7654709
Sonick Mumba <kingellie.mumba@gmail.com>
2024-05-15 17:46:55 +0200

    Add headerSlices files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        sonick
       </header>
     </div>
   );
---
+++ src/features/header/HeaderInput.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeaderInput = () => {
+  return (
+    <div>HeaderInput</div>
+  )
+}
+
+export default HeaderInput
