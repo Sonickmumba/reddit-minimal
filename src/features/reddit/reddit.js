c245b31368c557a957bd66eb0a1f039c735e0b25
Sonick Mumba <kingellie.mumba@gmail.com>
2024-05-16 19:17:50 +0200

    Add dummy data to use for the reddit component

--- src/features/reddit/reddit.js
+++ src/features/reddit/reddit.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import styles from './Reddit.module.css';
+/* eslint-disable react/prop-types */
 
-const Reddit = () => {
-  return (
-    <div>reddit</div>
-  )
-}
+const Reddit = ({
+  title, imageSrc, postedBy, daysPosted, numMessages,
+}) => (
+  <div className={styles.reddit}>
+    {' '}
+    <h2 className={styles.title}>{title}</h2>
+    {imageSrc && <img src={imageSrc} alt="Reddit post" className={styles.image} />}
+    <div className={styles.line} />
+    <div className={styles.footer}>
+      <div className={styles.userinfo}>
+        <img src="/path/to/small-pic.png" alt="User" className={styles.userPic} />
+        <p className={styles.postedBy}>
+          Posted by
+          {postedBy}
+          {' '}
+          {daysPosted}
+          {' '}
+          days ago
+        </p>
+      </div>
+      <div className={styles.messages}>
+        <i className="fa fa-envelope" />
+        {' '}
+        {numMessages}
+      </div>
+    </div>
+  </div>
+);
 
-export default Reddit
+export default Reddit;
