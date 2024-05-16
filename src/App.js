30ede6b21fb0e7f6f30217dcf63a3294e10fb98c
Sonick Mumba <kingellie.mumba@gmail.com>
2024-05-16 19:40:27 +0200

    Add image urls

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 // import logo from './logo.svg';
 // import { Counter } from './features/counter/Counter';
 import Header from './features/header/Header';
+import Reddit from './features/reddit/Reddit';
 import './App.css';
 
 function App() {
   return (
     <div className="App-header">
       <Header />
+      <Reddit />
     </div>
   );
 }
