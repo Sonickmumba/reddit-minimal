ab1485d67d2d7a08e84abf809505d70cc61e1a32
Sonick Mumba <kingellie.mumba@gmail.com>
2024-05-18 13:47:12 +0200

    Add icons for the subreddits

--- src/features/subreddit/Subreddit.js
+++ src/features/subreddit/Subreddit.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import styles from './Subreddit.module.css';
+import football from './football.svg';
 
 const Subreddit = () => (
   <div className={styles.subredditList}>
     <h2 className={styles.title}>Subreddits</h2>
     <ul className={styles.list}>
       <li className={styles.subredditItem}>
+        <span className="material-symbols-outlined">home</span>
         <a href="/home">Home</a>
       </li>
       <li className={styles.subredditItem}>
+        <span className="material-symbols-outlined">
+          sentiment_satisfied
+        </span>
         <a href="/home">Funny</a>
       </li>
       <li className={styles.subredditItem}>
+        <span className="material-symbols-outlined">
+          hd
+        </span>
         <a href="/home">Movies</a>
       </li>
       <li className={styles.subredditItem}>
+        <span className="material-symbols-outlined">
+          stadia_controller
+        </span>
         <a href="/home">Gaming</a>
       </li>
       <li className={styles.subredditItem}>
+        <img src={football} alt="football icon" className={styles.football} />
         <a href="/home">Football</a>
       </li>
       <li className={styles.subredditItem}>
