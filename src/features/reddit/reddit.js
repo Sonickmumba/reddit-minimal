b4883d6b789bc4be4188b638df5f0375a43e31a4
Sonick Mumba <kingellie.mumba@gmail.com>
2024-05-19 19:11:31 +0200

    Update the subreddit

--- src/features/reddit/reddit.js
+++ src/features/reddit/reddit.js
@@ -1,42 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchPosts, selectFilteredPosts } from './redditSlice';
 import styles from './Reddit.module.css';
 import message from './message.svg';
 /* eslint-disable react/prop-types */
 import dummyPosts from './dummyPosts';
 
-const Reddit = () => (
-  <div className={styles.redditContainer}>
-    {dummyPosts.map((dummy) => (
-      <div className={styles.reddit} key={dummy.id}>
-        {' '}
-        <h2 className={styles.title}>{dummy.title}</h2>
-        {dummy.imageSrc && (
-          <img
-            src={dummy.imageSrc}
-            alt="Reddit post"
-            className={styles.image}
-          />
-        )}
-        <div className={styles.line} />
-        <div className={styles.footer}>
-          <div className={styles.userinfo}>
-            <img src={dummy.userPic} alt="User" className={styles.userPic} />
-            <p className={styles.postedBy}>
-              <span className={styles.namePostby}>{dummy.postedBy}</span>
-              {' '}
-              {dummy.daysPosted}
-              {' '}
-              days ago
-            </p>
-          </div>
-          <div className={styles.messages}>
-            <img src={message} alt="Message icon" className={styles.messageIcon} />
-            <span>{dummy.numMessages}</span>
+const Reddit = () => {
+  const dispatch = useDispatch();
+  const posts = useSelector(selectFilteredPosts);
+  const selectedSubreddit = useSelector(
+    (state) => state.reddit.selectedSubreddit,
+  );
+
+  console.log(posts);
+
+  useEffect(() => {
+    dispatch(fetchPosts(selectedSubreddit));
+  }, [dispatch, selectedSubreddit]);
+
+  return (
+    <div className={styles.redditContainer}>
+      {dummyPosts.map((dummy) => (
+        <div className={styles.reddit} key={dummy.id}>
+          {' '}
+          <h2 className={styles.title}>{dummy.title}</h2>
+          {dummy.imageSrc && (
+            <img
+              src={dummy.imageSrc}
+              alt="Reddit post"
+              className={styles.image}
+            />
+          )}
+          <div className={styles.line} />
+          <div className={styles.footer}>
+            <div className={styles.userinfo}>
+              <img src={dummy.userPic} alt="User" className={styles.userPic} />
+              <p className={styles.postedBy}>
+                <span className={styles.namePostby}>{dummy.postedBy}</span>
+                {' '}
+                {dummy.daysPosted}
+                {' '}
+                days ago
+              </p>
+            </div>
+            <div className={styles.messages}>
+              <img
+                src={message}
+                alt="Message icon"
+                className={styles.messageIcon}
+              />
+              <span>{dummy.numMessages}</span>
+            </div>
           </div>
         </div>
-      </div>
-    ))}
-  </div>
-);
+      ))}
+    </div>
+  );
+};
 
 export default Reddit;
--- src/features/subreddit/Subreddit.js
+++ src/features/subreddit/Subreddit.js
@@ -1,41 +1,56 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './Subreddit.module.css';
 import football from './football.svg';
 
-const Subreddit = () => (
-  <div className={styles.subredditList}>
+// const Subreddit = () => (
+//   <div className={styles.subredditList}>
+//     <h2 className={styles.title}>Subreddits</h2>
+//     <ul className={styles.list}>
+//       <li className={styles.subredditItem}>
+//         <span className="material-symbols-outlined">home</span>
+//         <a href="/home">Home</a>
+//       </li>
+//       <li className={styles.subredditItem}>
+//         <span className="material-symbols-outlined">sentiment_satisfied</span>
+//         <a href="/home">Funny</a>
+//       </li>
+//       <li className={styles.subredditItem}>
+//         <span className="material-symbols-outlined">hd</span>
+//         <a href="/home">Movies</a>
+//       </li>
+//       <li className={styles.subredditItem}>
+//         <span className="material-symbols-outlined">stadia_controller</span>
+//         <a href="/home">Gaming</a>
+//       </li>
+//       <li className={styles.subredditItem}>
+//         <img src={football} alt="football icon" className={styles.football} />
+//         <a href="/home">Football</a>
+//       </li>
+//       <li className={styles.subredditItem}>
+//         <span className="material-symbols-outlined">currency_pound</span>
+//         <a href="/home">Politics</a>
+//       </li>
+//       <li className={styles.subredditItem}>
+//         <span className="material-symbols-outlined">breaking_news</span>
+//         <a href="/home">News</a>
+//       </li>
+//     </ul>
+//   </div>
+// );
+
+const Subreddit = () => {
+  return ((
+    <div className={styles.subredditList}>
     <h2 className={styles.title}>Subreddits</h2>
     <ul className={styles.list}>
       <li className={styles.subredditItem}>
         <span className="material-symbols-outlined">home</span>
         <a href="/home">Home</a>
       </li>
-      <li className={styles.subredditItem}>
-        <span className="material-symbols-outlined">sentiment_satisfied</span>
-        <a href="/home">Funny</a>
-      </li>
-      <li className={styles.subredditItem}>
-        <span className="material-symbols-outlined">hd</span>
-        <a href="/home">Movies</a>
-      </li>
-      <li className={styles.subredditItem}>
-        <span className="material-symbols-outlined">stadia_controller</span>
-        <a href="/home">Gaming</a>
-      </li>
-      <li className={styles.subredditItem}>
-        <img src={football} alt="football icon" className={styles.football} />
-        <a href="/home">Football</a>
-      </li>
-      <li className={styles.subredditItem}>
-        <span className="material-symbols-outlined">currency_pound</span>
-        <a href="/home">Politics</a>
-      </li>
-      <li className={styles.subredditItem}>
-        <span className="material-symbols-outlined">breaking_news</span>
-        <a href="/home">News</a>
-      </li>
     </ul>
   </div>
-);
+  ))
+}
 
 export default Subreddit;
