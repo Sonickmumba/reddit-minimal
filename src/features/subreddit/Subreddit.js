d92c5f7448e1b0ef5dea1296b83443c9452c8810
Sonick Mumba <kingellie.mumba@gmail.com>
2024-05-21 01:11:48 +0200

    Remove commented code

--- src/features/subreddit/Subreddit.js
+++ src/features/subreddit/Subreddit.js
@@ -5,42 +5,6 @@ import { setSelectedSubreddit } from '../reddit/redditSlice';
 import styles from './Subreddit.module.css';
 // import football from "./football.svg";
 
-// const Subreddit = () => (
-//   <div className={styles.subredditList}>
-//     <h2 className={styles.title}>Subreddits</h2>
-//     <ul className={styles.list}>
-//       <li className={styles.subredditItem}>
-//         <span className="material-symbols-outlined">home</span>
-//         <a href="/home">Home</a>
-//       </li>
-//       <li className={styles.subredditItem}>
-//         <span className="material-symbols-outlined">sentiment_satisfied</span>
-//         <a href="/home">Funny</a>
-//       </li>
-//       <li className={styles.subredditItem}>
-//         <span className="material-symbols-outlined">hd</span>
-//         <a href="/home">Movies</a>
-//       </li>
-//       <li className={styles.subredditItem}>
-//         <span className="material-symbols-outlined">stadia_controller</span>
-//         <a href="/home">Gaming</a>
-//       </li>
-//       <li className={styles.subredditItem}>
-//         <img src={football} alt="football icon" className={styles.football} />
-//         <a href="/home">Football</a>
-//       </li>
-//       <li className={styles.subredditItem}>
-//         <span className="material-symbols-outlined">currency_pound</span>
-//         <a href="/home">Politics</a>
-//       </li>
-//       <li className={styles.subredditItem}>
-//         <span className="material-symbols-outlined">breaking_news</span>
-//         <a href="/home">News</a>
-//       </li>
-//     </ul>
-//   </div>
-// );
-
 const Subreddit = () => {
   const dispatch = useDispatch();
   const subreddits = useSelector(selectSubreddits);
@@ -72,7 +36,15 @@ const Subreddit = () => {
       <ul className={styles.list}>
         {subreddits.map((subreddit) => (
           <li className={styles.subredditItem} key={subreddit.id}>
-            <span className="material-symbols-outlined">home</span>
+            <img
+              src={
+                subreddit.icon_img
+                || `https://api.adorable.io/avatars/256/${subreddit.display_name}.png`
+              }
+              alt="subreddit icon"
+              className={styles.subredditIcons}
+            />
+            {/* <span className="material-symbols-outlined">home</span> */}
             <a
               href="/home"
               onClick={() => dispatch(setSelectedSubreddit(subreddit.url))}
